3e80a9541ad546e367cc8dfa46719e3604f751a5
Sumeet Sharma <sumeet.sharma@berkeley.edu>
2020-06-28 17:59:09 -0700

    Added image storage backend Cloudinary for uploads, viewing and deleting photos. No distinction between environments.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cloudinary = require('cloudinary').v2;
 const localhostPort = 3000
 const ejs = require('ejs')
 const fs = require('fs')
@@ -7,7 +8,9 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const templatePath = __dirname + '/index.ejs'
 const PORT = process.env.PORT || localhostPort
+const IMAGE_FOLDER = 'images'
 
+// Static assets and parser setup, template file read
 app.use(express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/css'));
 app.use(fileUpload({
@@ -17,39 +20,59 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var templ = fs.readFileSync(templatePath, 'utf8');
 
-app.get('/', (req, res) => {	
-	res.send(ejs.render(templ, {
-		images: getImages()}
-))})
+// Cloudinary config
+cloudinary.config({
+  cloud_name: 'sumeets14',
+  api_key: '757469232411646',
+  api_secret: '16TPOSAYcqiX9PHD2sXVDy_rJgc'
+});
 
-function getImages() {
-	var images = [];
-	fs.readdirSync(__dirname + '/images').forEach(function (file) {
-	    // Do whatever you want to do with the file
-		if (file != '.gitignore') {
-			images.push({path: file})
-		}
+
+// Routes setup
+
+app.get('/', (req, res) => getImagesResponse(res))
+
+function getImagesResponse(res) {
+	cloudinary.api.resources({type: 'upload', prefix: IMAGE_FOLDER},
+	  function(error, result) {
+		  console.log("result: %s, error: %s", result, error);
+		  var images = [];
+		  if (result) {
+			  result.resources.forEach(image => {
+				  console.log(image);
+				  images.push({url: image.url});
+			  });
+		  }
+		  
+	  	  res.send(ejs.render(templ, {
+			  images: images}));
 	});
-	console.log("images: " + images);
-	return images;
 }
 
 app.post('/', (req, res) => {
+	// Save image to temp location in /images
 	console.log('Uploaded file');
 	console.log(req.files);
 	let file = req.files.file;
 	file.mv('./images/' + file.name);
+	
+	// Now upload to cloudinary
+    cloudinary.uploader.upload('./images/' + file.name, {folder:IMAGE_FOLDER},
+    function(error, result) {console.log("result: %s, error: %s", result, error); });
+	
+	
 	res.redirect('/');
 })
 
 app.post('/clear', (req, res) => {
-	fs.readdirSync(__dirname + '/images').forEach(function (file) {
-	        // Do whatever you want to do with the file
-		if (file != '.gitignore') {
-			fs.unlinkSync('images/' + file);
-	    }
+	cloudinary.api.delete_resources_by_prefix(IMAGE_FOLDER, (error, result) => {
+		if (error) {
+			console.log("Error deleting all images/*")
+		} else {
+			console.log("Result of deleting images/*: %s", result)
+		}
+	    res.redirect('/');	
 	});
-	res.redirect('/');
 })
 
 app.listen(PORT, () => console.log('Image upload viewer app listening at http://localhost:%s', PORT))
